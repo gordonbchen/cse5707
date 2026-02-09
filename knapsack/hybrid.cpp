20c829770f7e40919cfad8c68a66df90e2b8eff0
jdwl-7321 <juan.wulin2023@gmail.com>
2026-02-08 21:35:59 -0500

    1) reverted to using ratio for calculation in frac_ks_rev ======= 2) removed comments ======== 3) added a dfs function (better approximations, when we can't solve). approximation for corr50k is optimal.

--- knapsack/hybrid.cpp
+++ knapsack/hybrid.cpp
@@ -17,7 +17,6 @@ struct Item {
     double r;
 };
 
-// CHANGE 1: int ks_greedy --> long long ks_greedy (since sum of values can overflow)
 long long ks_greedy(const std::vector<Item>& items, int c, int N, int* remc, bool &early_stop) {
     auto iterator = std::upper_bound(PS_WEIGHTS.begin(), PS_WEIGHTS.end(), c);
     int idx = std::distance(PS_WEIGHTS.begin(), iterator) - 1;
@@ -30,7 +29,6 @@ long long ks_greedy(const std::vector<Item>& items, int c, int N, int* remc, boo
         return value;
     }
 
-    // fill the rest of capacity to tighten bound
     for (int i = idx + 1; i < N; ++i) {
         if (items[i].w <= *remc) {
             value += items[i].v;
@@ -41,44 +39,33 @@ long long ks_greedy(const std::vector<Item>& items, int c, int N, int* remc, boo
     return value;
 }
 
-// CHANGE 2: int frac_ks --> long long frac_ks (since sum of values can overflow)
 long long frac_ks(const std::vector<Item>& items, int item_i, int c, int N, int* endi, int* remc) {
     long long current_weight_offset = PS_WEIGHTS[item_i];
     long long target_weight = current_weight_offset + c;
 
-    // binary search
     auto iterator = std::upper_bound(PS_WEIGHTS.begin() + item_i, PS_WEIGHTS.end(), target_weight);
     int idx = std::distance(PS_WEIGHTS.begin(), iterator) - 1;
 
-    // sum of fully taken items
-    // remove double casting
     long long solution = (PS_VALUES[idx] - PS_VALUES[item_i]);
 
     if (idx == N) {
         *endi = N;
         *remc = c - (int)(PS_WEIGHTS[N] - PS_WEIGHTS[item_i]);
-        // CHANGE 3: (int)solution --> (long long)solution (same rationale as changes 1,2)
         return (long long)solution;
     }
 
-    // update ending idx
     *endi = idx;
 
     long long weight_used = PS_WEIGHTS[idx] - PS_WEIGHTS[item_i];
     int local_rem = c - (int) weight_used;
 
-    // add fractional if exists
-    // rem * ratio --> rem * value / weight. fixes some floating pt errors
-    // (check data/floatingpoint.txt)
     solution += ((long long)local_rem*items[idx].v) / items[idx].w;
 
     *remc = local_rem;
     return solution;
 }
 
-// CHANGE 5: int frac_ks_rev --> long long frac_ks_rev
 long long frac_ks_rev(const std::vector<Item>& items, int starti, int endi, int N, int c, int remc) {
-    // double --> long long
     long long sol = 0;
     if (endi == N) {  // end of items, leftover capacity.
         if (remc >= c) { return 0; }
@@ -86,7 +73,7 @@ long long frac_ks_rev(const std::vector<Item>& items, int starti, int endi, int 
     }
     else if (remc > 0) {  // fractional was used.
         if (remc >= c) {
-            return ((long long)c * items[endi].v) / items[endi].w;
+            return (long long)(c * items[endi].r);
         }
         sol += (long long)(remc * items[endi].r);
         c -= remc;
@@ -98,16 +85,12 @@ long long frac_ks_rev(const std::vector<Item>& items, int starti, int endi, int 
             sol += items[i].v;
             continue;
         }
-        // CHANGE 6: (int)(c * items[i].r) --> ((long long)c*items[i].v) / items[i].w
-        // prevents some floating point errors
-        sol += ((long long)c*items[i].v) / items[i].w;
+        sol += (long long)(c*items[i].r);
         break;
     }
-    // CHANGE 7: (int)sol --> (long long)sol
     return sol;
 }
 
-// NEW 1: added dp solution for some cases BB can't solve
 long long dp(const std::vector<Item>& items, int C, int N) {
     std::vector<long long> prev_dp(C+1, 0);
     std::vector<long long> curr_dp(C+1, 0);
@@ -131,7 +114,72 @@ long long dp(const std::vector<Item>& items, int C, int N) {
     return prev_dp[C];
 }
 
-// CHANGE 11: int --> long long
+long long dfs(const std::vector<Item>& items, int C, int N, long long lb, int &nodes_limit, int &is_complete) {
+    using Node = std::tuple<long long, int, long long, int>;  // ub, item_i, v, c
+    std::vector<Node> stack;
+    stack.reserve(3*N);
+    stack.push_back({lb + 1, 0, 0, C});
+
+    long long cand_ub;
+    int endi, remc, nodes_processed;
+    nodes_processed = 0;
+
+    while (!stack.empty()) {
+        // force stop
+        if (++nodes_processed >= nodes_limit) {
+            is_complete = 0;
+            return lb;
+        }
+
+        auto [ub, item_i, v, c] = stack.back();
+        stack.pop_back();
+
+        // continue instead of break
+        if (ub <= lb) { continue; }
+
+        if (item_i == (N-1)) {
+            if (c >= items[item_i].w) {
+                v += items[item_i].v;
+            }
+            lb = std::max(lb, v);
+            continue;
+        }
+
+        // use take_endi, remc to save state for take branch
+        int take_endi, take_remc;
+        cand_ub = v + frac_ks(items, item_i+1, c, N, &endi, &remc);
+        take_endi = endi;
+        take_remc = remc;
+
+        // for the exclusion branch, exclude contiguous items w/ same value
+        int next_i = item_i + 1;
+        while (next_i < N && items[next_i].v == items[item_i].v && items[next_i].w == items[item_i].w) {
+            next_i++;
+        }
+
+        long long ub_skip;
+        if (next_i == item_i+1) {
+            ub_skip = cand_ub;
+        }
+        else {
+            ub_skip = v + frac_ks(items, next_i, c, N, &endi, &remc);
+        }
+
+        if (ub_skip > lb) {
+            stack.push_back({ub_skip, next_i, v, c});
+        }
+
+        if (c < items[item_i].w) { continue; }
+        v += items[item_i].v;
+        lb = std::max(lb, v);
+        c -= items[item_i].w;
+        cand_ub += items[item_i].v - frac_ks_rev(items, item_i+1, take_endi, N, items[item_i].w, take_remc);
+        if (cand_ub > lb) {
+            stack.push_back({cand_ub, item_i+1, v, c});
+        }
+    }
+    return lb;
+}
 long long knapsack(int &is_complete) {
     // Parse data.
     int N;
@@ -163,23 +211,19 @@ long long knapsack(int &is_complete) {
     // Init lb.
     int endi, remc;
     bool early_stop = false;
-    // CHANGE 8: double --> long long
     long long lb = ks_greedy(items, C, N, &remc, early_stop);
     if (early_stop) { return lb; }
 
-    // NEW 3: add additional early stop
     // calculate global upperbound, if global upperbound <= LB, then LB is optimal
     // (early stop 2)
     long long ub_start = frac_ks(items, 0, C, N, &endi, &remc);
     if (ub_start <= lb) { return lb; }
 
     // Solve.
-    // CHANGE 9: std::tuple<double, int, int, int> --> <long long, int, long long, int>
     using Node = std::tuple<long long, int, long long, int>;  // ub, item_i, v, c.
     std::priority_queue<Node, std::vector<Node>> pq;
     pq.push({lb + 1, 0, 0, C});
 
-    // CHANGE 10: int --> long long
     long long cand_ub;
     int nodes_processed = 0;
 
@@ -190,7 +234,10 @@ long long knapsack(int &is_complete) {
             if (C <= 500'000) { return dp(items, C, N); }
             // return an approximation, and set is_complete to false
             is_complete = 0;
-            return lb;
+            int dfs_nodes_limit = 5'000'000;
+            // pivot to dfs first, for possibly a better approximation
+            // since best first likely gone into a branch in depth
+            return dfs(items, C, N, lb, dfs_nodes_limit, is_complete);
         }
         auto [ub, item_i, v, c] = pq.top();
         pq.pop();
@@ -219,28 +266,9 @@ long long knapsack(int &is_complete) {
         }
 
         long long ub_skip;
-        // fix the skip bug
-        // small performance fix
-        // BEFORE:
-        //      used cand_ub for skip node
-        //      which is calculated at (item_i+1), if we skip dupes and jump to next_i
-        //      but cand_ub still includes the values of skipped dupes
-        //      which gives the skip branch a fake, higher upperbound, preventing
-        //      less pruning
-        // AFTER:
-        //      if we skip dupes (next_i > item_i+1), we recalculate the bound at next_i
-        //      this gives tigher upper bound
-        // EX:
-        //      items = [A (v=10), B (v=10), C (v=10), D (v=1)]
-        //      capacity allows 1 item
-        //      skip A --> skip B and C (dupe), next is D
-        //      -- old bound calculated at B, which includes B&C --> lb = 20
-        //      -- new bound calculated at D --> bound = 1
-        // if no duplicates were skipped, bound is the same as cand_ub (most likely)
         if (next_i == item_i+1) {
             ub_skip = cand_ub;
         }
-        // otherwise, recalculate the bound
         else {
             ub_skip = v + frac_ks(items, next_i, c, N, &endi, &remc);
         }
