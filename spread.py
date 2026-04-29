3b1c6a3af93d79733d5e83544f3419223d2d3594
gordonbchen <gordonbchen@gmail.com>
2026-04-29 01:42:32 -0400

    sum interval idx

--- spread.py
+++ spread.py
@@ -1,8 +1,8 @@
 import math
 
 
-def calc_intervals(xs: list[tuple[int, int]]) -> tuple[list[tuple[int, int]], list[int], list[int],
-                                                       list[int], list[int], list[int], list[tuple[int, int]]]:
+def calc_intervals(xs: list[tuple[int, int]], s: int) -> tuple[list[tuple[int, int]], list[int], list[int],
+                                                               list[int], list[int], list[int], list[tuple[int, int]], int]:
     bs = sorted([x[i] for x in xs for i in range(2)])  # sorted bounds.
     bs = list(set(bs))
     print(f"\nbs: {bs}")
@@ -52,45 +52,27 @@ def calc_intervals(xs: list[tuple[int, int]]) -> tuple[list[tuple[int, int]], li
     print(f"\nes: {es}")
     print(f"es2: {es2}")
 
-    sum_ranges = [(es[i] + n_in[i] * imin, es[i] + n_in[i] * imax) for i, (imin, imax) in enumerate(intervals)]
+    sum_ranges = []
+    sum_interval_idx = -1  # TODO: this can be binary searched.
+    for i, (imin, imax) in enumerate(intervals):
+        sum_min, sum_max = (es[i] + n_in[i] * imin, es[i] + n_in[i] * imax)
+        sum_ranges.append((sum_min, sum_max))
+        if s in range(sum_min, sum_max+1):
+            sum_interval_idx = i
+            break
+    assert sum_interval_idx > 0, "Failed to find sum in sum intervals."
     print(f"\nsum_ranges: {sum_ranges}")
-    return intervals, n_in, n_left, n_right, es, es2, sum_ranges
-
-
-def calc_min_ssq(s: int, sum_ranges: list[tuple[int, int]], n_in: list[int], es: list[int], es2: list[int]) -> tuple[float, float]:
-    min_ssq = float("inf")
-    for i, sum_range in enumerate(sum_ranges):
-        if not s in range(*sum_range):
-            continue
-
-        v = (s - es[i]) / n_in[i]
-        sum_squares = es2[i] + n_in[i] * v*v
-        min_ssq = min(sum_squares, min_ssq)
-    return min_ssq, v
-
-
-def calc_min_ssz(s: int, sum_ranges: list[tuple[int, int]], n_in: list[int], es: list[int], es2: list[int]) -> tuple[int, float]:
-    min_ssz = float("inf")
-    for i, sum_range in enumerate(sum_ranges):
-        if not s in range(*sum_range):
-            continue
-
-        v = (s - es[i]) / n_in[i]
-        v_plus = (s - es[i]) % n_in[i]
-        v_minus = (n_in[i] - v_plus) % n_in[i]
-
-        sum_squares = es2[i] + v_plus * math.ceil(v)**2 + v_minus * math.floor(v)**2
-        min_ssz = min(sum_squares, min_ssz)
-    return min_ssz, v
+    print(f"sum_interval_idx: {sum_interval_idx}")
+    return intervals, n_in, n_left, n_right, es, es2, sum_ranges, sum_interval_idx
 
 
 def clamp(x, l, u):
     return min(max(x, l), u)
 
 
-# TODO: finish.
+# TODO: finish, start at sum interval idx.
 def update_x_max(intervals: list[tuple[int, int]], n_in: list[int], n_left: list[int], n_right: list[int], es: list[int],
-                 es2: list[int], sum_ranges: list[tuple[int, int]], x: tuple[int, int], vq: float) -> int:
+                 es2: list[int], sum_ranges: list[tuple[int, int]], s: int, x: tuple[int, int], vq: float) -> int:
     x_min, x_max = x
     for i_min, i_max in intervals:
         # No filtering if x is in the left since x_max is the value.
@@ -99,23 +81,28 @@ def update_x_max(intervals: list[tuple[int, int]], n_in: list[int], n_left: list
 
 
 if __name__ == "__main__":
-    xs = [(1, 3), (2, 6), (3, 9)]
+    xs = [(1, 3), (2, 6), (3, 9)]  # x bounds.
     print(f"xs: {xs}")
-
-    intervals, n_in, n_left, n_right, es, es2, sum_ranges = calc_intervals(xs)
 
     s = 10  # target sum.
     print(f"\ns: {s}")
 
-    min_ssq, vq = calc_min_ssq(s, sum_ranges, n_in, es, es2)
-    assert min_ssq == 33.5
-    print(f"\nmin_ssq: {min_ssq}")
-    print(f"vq: {vq}")
+    intervals, n_in, n_left, n_right, es, es2, sum_ranges, sum_interval_idx = calc_intervals(xs, s)
 
-    vs = [clamp(vq, l, u) for (l, u) in xs]
+    # Calculate the v Q center.
+    v = (s - es[sum_interval_idx]) / n_in[sum_interval_idx]
+    print(f"\nv: {v}")
+
+    min_ssq = es2[sum_interval_idx] + n_in[sum_interval_idx] * v*v
+    print(f"min_ssq: {min_ssq}")
+    assert min_ssq == 33.5
+
+    vs = [clamp(v, l, u) for (l, u) in xs]
     print(f"vs: {vs}")
 
-    min_ssz, vz = calc_min_ssz(s, sum_ranges, n_in, es, es2)
-    assert min_ssz == 34
+    # Calculate the v Z center.
+    v_plus = (s - es[sum_interval_idx]) % n_in[sum_interval_idx]
+    v_minus = (n_in[sum_interval_idx] - v_plus) % n_in[sum_interval_idx]
+    min_ssz = es2[sum_interval_idx] + v_plus * math.ceil(v)**2 + v_minus * math.floor(v)**2
     print(f"\nmin_ssz: {min_ssz}")
-    print(f"vz: {vz}")
+    assert min_ssz == 34
