183282dd875d1d1ce7ab02bcee28d93a49f39abe
gordonbchen <gordonbchen@gmail.com>
2026-04-29 18:18:17 -0400

    spread dem legs

--- spread.py
+++ spread.py
@@ -70,14 +70,73 @@ def clamp(x, l, u):
     return min(max(x, l), u)
 
 
-# TODO: finish, start at sum interval idx.
-def update_x_max(intervals: list[tuple[int, int]], n_in: list[int], n_left: list[int], n_right: list[int], es: list[int],
-                 es2: list[int], sum_ranges: list[tuple[int, int]], s: int, x: tuple[int, int], vq: float) -> int:
+def update_x_max(
+    intervals: list[tuple[int, int]], n_in: list[int], n_left: list[int], n_right: list[int], es: list[int],
+    es2: list[int], s: int, sum_interval_idx: int, max_ss: float, x: tuple[int, int], v: float
+) -> int:
     x_min, x_max = x
-    for i_min, i_max in intervals:
-        # No filtering if x is in the left since x_max is the value.
-        if x_max <= i_min:
+    x_cur = v
+
+    # Cannot filter if x in left.
+    if x_max <= intervals[sum_interval_idx][0]:
+        return x_max
+
+    for i in reversed(range(sum_interval_idx+1)):
+        # x max is bound consistent if value is max. Cannot filter max more.
+        if x_cur >= x_max:
             return x_max
+
+        m, esi, es2i = update_interval_stats(x_cur, x, intervals[i], n_in[i], es[i], es2[i])
+
+        # TODO: how does this case work?
+        if m <= 0:
+            continue
+
+        # solve for maximum d can be increased by without violating max sum of squares.
+        a = 1 + 1/m
+        v_new = (s - esi) / m
+        b = 2 * (x_min - v_new)
+        sum_squares = es2i + m*v_new*v_new
+        c = sum_squares - max_ss
+        d_opt = (-b + (b*b - 4*a*c)**0.5) / (2*a)  # TODO: negative in sqrt???
+
+        interval_sum_min = esi + m*intervals[i][0]
+        d_max = s - interval_sum_min  # maximum d can be increased by before v too small for interval.
+        if d_opt > d_max:
+            x_cur += d_max
+            continue  # go down to next interval to keep increasing d.
+        x_cur += d_opt
+
+        # compute the maximum integral x.
+        x_cur = math.floor(x_cur)
+        m, esi, es2i = update_interval_stats(x_cur, x, intervals[i], n_in[i], es[i], es2[i])
+        v_new = (s - esi) / m
+        v_plus = (s - esi) % m
+        v_minus = m - v_plus
+        sum_squares = es2i + v_plus * (math.ceil(v_new)**2) + v_minus * (math.floor(v_new)**2)
+        while sum_squares > max_ss:
+            sum_squares += 2 * (math.ceil(v_new) - x_cur)  # TODO: wtf?
+            x_cur -= 1
+        break
+    return min(x_cur, x_max)
+
+
+def update_interval_stats(
+    x_cur: float, x: tuple[int, int], interval: tuple[int, int], m: int, esi: int, es2i: int
+) -> tuple[int, float, float]:
+    x_min, x_max = x
+    i_min, i_max = interval
+
+    d = x_cur - x_min
+    esi += d
+    es2i += d*d + 2*d*x_min
+
+    # Split interval (if x in M).
+    if x_max > i_min and x_min < i_max:
+        m -= 1
+        esi += x_min
+        es2i += x_min * x_min
+    return m, esi, es2i
 
 
 if __name__ == "__main__":
@@ -90,19 +149,24 @@ if __name__ == "__main__":
     intervals, n_in, n_left, n_right, es, es2, sum_ranges, sum_interval_idx = calc_intervals(xs, s)
 
     # Calculate the v Q center.
-    v = (s - es[sum_interval_idx]) / n_in[sum_interval_idx]
-    print(f"\nv: {v}")
+    vq = (s - es[sum_interval_idx]) / n_in[sum_interval_idx]
+    print(f"\nvq: {vq}")
 
-    min_ssq = es2[sum_interval_idx] + n_in[sum_interval_idx] * v*v
+    min_ssq = es2[sum_interval_idx] + n_in[sum_interval_idx] * vq*vq
     print(f"min_ssq: {min_ssq}")
     assert min_ssq == 33.5
 
-    vs = [clamp(v, l, u) for (l, u) in xs]
+    vs = [clamp(vq, l, u) for (l, u) in xs]
     print(f"vs: {vs}")
 
     # Calculate the v Z center.
     v_plus = (s - es[sum_interval_idx]) % n_in[sum_interval_idx]
     v_minus = (n_in[sum_interval_idx] - v_plus) % n_in[sum_interval_idx]
-    min_ssz = es2[sum_interval_idx] + v_plus * math.ceil(v)**2 + v_minus * math.floor(v)**2
+    min_ssz = es2[sum_interval_idx] + v_plus * math.ceil(vq)**2 + v_minus * math.floor(vq)**2
     print(f"\nmin_ssz: {min_ssz}")
     assert min_ssz == 34
+
+    max_ss = 39
+    for x, v in zip(xs, vs):
+        new_x_max = update_x_max(intervals, n_in, n_left, n_right, es, es2, s, sum_interval_idx, max_ss, x, vq)
+        print(x, new_x_max)
